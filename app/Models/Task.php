1bb476d6a2f3ea7d60d823ae02c9466c70e1d9d4
Minh Cat DO <m.do@myskolae.fr>
2025-11-30 12:25:58 +0100

    model and migration of projects and tasks

---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'done',
+        'priority',
+        'due_at',
+    ];
+
+    // Relationship
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    // optional: cast done to boolean and due_at to datetime
+    protected $casts = [
+        'done' => 'boolean',
+        'due_at' => 'datetime',
+    ];
+}
---
+++ database/migrations/2025_11_30_111740_project_creation.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->enum('status', ['in_progress', 'completed', 'on_hold'])->default('on_hold');
+            $table->timestamp('due_at')->nullable();
+            $table->foreignId('owner_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
